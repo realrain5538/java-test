6702b959e8201a588cfe5a5786ae7ac30ea2b581
realrain5538 <bizrealrain@gmail.com>
2025-07-10 23:15:54 +0900

    HomeWork

--- src/Main.java
+++ src/Main.java
@@ -10,14 +10,68 @@ public class Main {
         int c = x * y; //곱하기
         int d =  x/y; //나누기
         int e =  x + 3; //대입 연산자
-//        boolean f = x > y; //논리 연산자
+        boolean f = x == 10; // 비교 연산자
+        boolean g = x > y; //논리 연산자
+        int h = x++; //증감 연산자
+        String i = (x > 2) ? "A" : "B"; //삼항 연산자
 
         System.out.println("더하기 : " + a);
         System.out.println("빼기 : " + b);
         System.out.println("곱하기 : " + c);
         System.out.println("나누기 : " + d);
         System.out.println("대입 연산자 : " + e);
-        System.out.println(x == 10); // 비교 연산자
+        System.out.println("비교 연산자 : " + f);
+        System.out.println("논리 연산자 : " + g);
+        System.out.println("증감 연산자 : " + h);
+        System.out.println("삼항 연산자 : " + i);
 
+        //if
+        int num = 10;
+
+        if(num > 0){
+            System.out.println("양수");
+        }else if(num == 0){
+            System.out.println("0");
+        }else{
+            System.out.println("음수");
+        }
+
+        //switch
+        int day = 3;
+
+        switch (day){
+            case 1 :
+                System.out.println("월");
+                break;
+            case 2 :
+                System.out.println("화");
+                break;
+            case 3 :
+                System.out.println("수");
+                 break;
+            default:
+                System.out.println("기타");
+                break;
+        }
+
+        //for
+        for(int n = 0; n < 5; n++){
+            System.out.println("for문 : " + n);
+        }
+
+        //while
+        int count = 3;
+
+        while (count > 0){
+            System.out.println("while문" + count);
+            count--;
+        }
+
+        //do-while
+        int testNum = 0;
+        do {
+            System.out.println("do while문" + testNum);
+            testNum++;
+        }while (testNum < 3);
     }
 }
